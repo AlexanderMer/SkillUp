4ef92289202532fa19e5b4d07b2bb20854512569
alexander <alexandermerabov@gmail.com>
2016-02-03 01:44:48 +0400

    Changed range() parameters to reduce calculations
    Now I generate needed sequence directly by calling range(1, 10)instead
    of generating from 0to 10 and adding 1 in each iteration of the list.

--- Python/Homeworks/hw3/D.py
+++ Python/Homeworks/hw3/D.py
@@ -1,9 +1,9 @@
-for row in range(9):
-    print(' ' * (10 - row + 1), end = '')
-    print(*(range(1, row + 1)), end = '', sep = '')
-    print(*range(row + 1, 0, -1), sep = '')
+for row in range(1, 10):
+    print(' ' * (10 - row), end = '')
+    print(*(range(1, row)), end = '', sep = '')
+    print(*range(row, 0, -1), sep = '')
     
-for row in range(7, -1, -1):
-    print(' ' * (10 - row + 1), end = '')
-    print(*range(1, row + 1), end ='', sep = '')
-    print(* range(row + 1, 0, - 1), sep = '')
+for row in range(8, -1, -1):
+    print(' ' * (10 - row ), end = '')
+    print(*range(1, row), end ='', sep = '')
+    print(* range(row, 0, - 1), sep = '')
