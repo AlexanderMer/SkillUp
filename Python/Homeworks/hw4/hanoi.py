b4f49f35844a9de316688f68b25bc3d13db60312
alexander <alexandermerabov@gmail.com>
2016-02-02 20:33:08 +0400

    Cleaned the code up a bit and added a move counter
    Note to self: in functions must declare 'global variable_name' in order
    for Python to see it in global scope, otherwise it will see it as a new
    variable when you try to call it. An error like 'UnboundLocalError:
    local variable referenced before assignment' is likely in such case.

--- Python/Homeworks/hw4/hanoi.py
+++ Python/Homeworks/hw4/hanoi.py
@@ -1,12 +1,15 @@
 pegs = [[], [], []]
+moves = 0
 
 def hanoi(height):
 	pegs[0] = list(range(1, height + 1))
 	print(*pegs)
 	move(0, 2, height)
-
+	print(*pegs)
+	print('completed in %s moves' % str(moves))
 
 def move(from_peg, to_peg, n):
+	global moves
 	if n > 1:
 		temp = 3 - (from_peg + to_peg)
 		move(from_peg, temp, n - 1 )
@@ -15,7 +18,6 @@ def move(from_peg, to_peg, n):
 	else:
 		pegs[to_peg].append(pegs[from_peg][-1])
 		pegs[from_peg].pop()
+		moves += 1
 
-hanoi(5)
-print(*pegs)
-
+hanoi(6)
