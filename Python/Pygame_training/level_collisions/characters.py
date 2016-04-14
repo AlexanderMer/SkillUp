47ef0b1f99eb76382d3fdc7b8b7813cb797ddded
alexander <alexandermerabov@gmail.com>
2016-04-15 01:55:15 +0400

    Got rid of absolute class "Block" and added collisions with level sprites in Mage class

--- Python/Pygame_training/level_collisions/characters.py
+++ Python/Pygame_training/level_collisions/characters.py
@@ -3,32 +3,6 @@ from pygame.locals import *
 from name_tag import Name_tag
 
 
-class Block(pygame.sprite.Sprite):
-
-    def __init__(self, screen, min_size, max_size, colour=0):
-        super().__init__()
-        size = random.randint(min_size, max_size)
-        self.image = pygame.Surface((size, size))
-        self.image.fill(colour if colour != 0 else self._random_color())
-        self.screen = screen
-        self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = random.randint(0, 700), random.randint(0, 500)
-        self.speedx, self.speedy = random.randint(-17, 17), random.randint(-17, 17)
-
-    def update(self):
-        """This function must be called each frame for movement and collision testing"""
-        self.rect.left += self.speedx
-        self.rect.top += self.speedy
-        if self.rect.right > self.screen.get_width() or self.rect.left < 0:
-            self.speedx = -self.speedx
-        if self.rect.bottom > self.screen.get_height() or self.rect.top < 0:
-            self.speedy = -self.speedy
-
-    def _random_color(self):
-        """returns a tuple prepresenting a random color in the following format (r, g, b)"""
-        return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
-
 class Mage(pygame.sprite.Sprite):
 
     def __init__(self, screen, level, name):
@@ -48,6 +22,8 @@ class Mage(pygame.sprite.Sprite):
             K_d: (self.move_character, (self.movement_speed, 0))
         }
         self.name_tag = Name_tag(name, font_size=int(self.rect.height / 3))
+        self.collided = 0  # List of sprites which are in contact with player
+        self.curent_tile = 0  # Tile occupied by player at the moment
         self.spawn()
 
     def _load_image(self, path="sprites/mage.png"):
@@ -73,20 +49,30 @@ class Mage(pygame.sprite.Sprite):
             self.looking_right = not self.looking_right
 
     def press_key(self, key):
-        """Gets function from keys_map and calls it with arguments stored in same dictionary"""
+        """Key must be keyboard key defined in pygame.locals constants
+        Gets function from keys_map and calls it with arguments stored in same dictionary"""
         if key in self.keys_map:
             self.keys_map[key][0](self.keys_map[key][1])
 
     def update(self):
-        self.collided = pygame.sprite.spritecollide(self, self.level.sprite_group, 65)
+        self.collided = pygame.sprite.spritecollide(self, self.level.sprite_group, False)
+        # Check which tile player occupies now and if player tries to walk on unwalkable tile
+        jesus_walking = False
+        for tile in self.collided:
+            if (tile.rect.left < self.rect.center[0] < tile.rect.right and
+                    tile.rect.bottom > self.rect.center[1] > tile.rect.top):
+                self.curent_tile = tile
+            if not tile.walkable:
+                jesus_walking = True
+        if jesus_walking:
+            self.rect.clamp_ip(self.curent_tile)
+
         self._check_pos()
         self.rect.clamp_ip(self.screen.get_rect())  # Doesn't allow player to move beyond the screen
         # Calculate Y coordinate so it's right above sprite's head
         self.name_tag.update_pos((self.rect.left, self.rect.center[1] - self.rect.height / 2))
         self.screen.blit(self.name_tag.marked_text_surface, self.name_tag.marked_text_rect)
         self.screen.blit(self.name_tag.text_surface, self.name_tag.text_rect)
-
-
 
     def _check_pos(self):
         """This method is responceble for all actions related to player position"""
@@ -99,7 +85,5 @@ class Mage(pygame.sprite.Sprite):
         if self.rect.y <= self.level.margin:
                 self.level.move_map((0, self.movement_speed))
 
-
     def spawn(self):
         self.rect.center = (random.randint(0, self.level.LEVEL_WIDTH_PX), random.randint(0, self.level.LEVEL_HEIGHT_PX))
-
