0b0f57db540e021d5b8d3cf810ddcd20afe9b6cd
alexander <alexandermerabov@gmail.com>
2016-04-21 13:33:45 +0400

    Added form for registering new users

--- Python/Django/connected/person/urls.py
+++ Python/Django/connected/person/urls.py
@@ -4,5 +4,6 @@ from person import views
 
 urlpatterns = (
     url(r'^$', views.index, name='home'),
-    url(r'^id(?P<profile_id>\d+)/$', views.details, name='details')
+    url(r'^id(?P<profile_id>\d+)/$', views.details, name='details'),
+    url(r'^register/', views.register, name='register')
 )
--- Python/Django/connected/person/views.py
+++ Python/Django/connected/person/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from person.models import UserProfile
+from person.forms import UserForm, UserProfileForm
+
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html", {})
+    profiles  = UserProfile.objects.all()
+    return render(request, "index.html", {'profiles': profiles})
 
 def details(request, profile_id):
     profile = UserProfile.objects.get(id=profile_id)
     return render(request, "details.html", {'profile': profile})
+
+def register(request):
+    if request.POST:
+        pass
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+        return render(request, 'register.html', {'user_form': user_form,
+                                                 'profile_form': profile_form})
